7a73a8d5e3cd2bea15063f1ee306da082b6f4a1b
Younes Piro <piroyounes148@gmail.com>
2022-04-24 05:56:42 +0000

    allows cors and enable csrf for graphql endpoint

--- appartements/urls.py
+++ appartements/urls.py
@@ -4,9 +4,10 @@ from .ETL.transform import clean
 from .ETL.extract import scrap
 from .ETL.load import load
 from appartements.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
     #path('', scrap)
     #path('',clean)
     #path('',load)
--- my_app/settings.py
+++ my_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5o@*@ae17d^6xiiea-)o)do1q%)hq!-)(h_phg&2v!s@s%bd_7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appartements',
     'graphene_django',
-    'django_elasticsearch_dsl'
+    'django_elasticsearch_dsl',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'my_app.urls'
 
